a7bdf035510173d01f1591fd4d3a85db6479d04d
danil <minakonn9shkoni@gmail.com>
2022-06-16 16:26:17 +0300

    Add prones_to_orders

--- sql/agregate.sql
+++ sql/agregate.sql
@@ -43,3 +43,49 @@ FROM "this_products"
 WHERE price >100
 GROUP BY brand;
 
+--@block
+DROP TABLE orders,phones;
+
+--@block
+CREATE TABLE orders (
+  id serial PRIMARY KEY,
+  "created_at" TIMESTAMP NOT NULL DEFAULT current_date,
+  "adress" VARCHAR(32),
+  "user_id" int REFERENCES users(id) ON DELETE CASCADE
+);
+
+--@block
+CREATE TABLE phones(
+  id serial PRIMARY KEY,
+  model VARCHAR(32) NOT NULL check(char_length(model)>=3),
+  brand VARCHAR(32) NOT NULL check(char_length(brand)>=3),
+  price DECIMAL(9,2) NOT NULL CHECK(price>0.00),
+  amount int NOT NULL  CHECK(amount>=0),
+
+  "create_at" TIMESTAMP NOT NULL DEFAULT current_date,
+  UNIQUE(model,brand)
+);
+
+
+--@block
+CREATE TABLE phones_to_orders(
+  order_id int REFERENCES orders(id),
+  phone_id int REFERENCES phones(id),
+  amount int NOT NULL CHECK(amount>0) DEFAULT 1,
+  PRIMARY KEY(order_id,phone_id)
+);
+
+
+--@block
+INSERT INTO phones(model,brand,price,amount)
+VALUES
+('3310','NOKIA',500,10),
+('3300','NOKIA',600,20),
+('Redmi 3','Xiaomi',1000,15),
+('Redmi 5','Xiaomi',1500,20),
+('Note 5','Xiaomi',2000,10),
+('Honor 1','Huawei',1500,10),
+('F12','Huawei',3000,40),
+('Galaxy','Samsung',1500,10),
+('A 51','Samsung',4000,15),
+('G20','OPPO',1500,20);
