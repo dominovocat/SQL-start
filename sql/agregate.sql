273ad1fcc8aec985581c59caee1dd3d595430d15
danil <minakonn9shkoni@gmail.com>
2022-06-17 15:19:35 +0300

    Correct

--- sql/agregate.sql
+++ sql/agregate.sql
@@ -114,50 +114,5 @@ SELECT order_id,
 FROM phones_to_orders
 ORDER BY amount DESC;
 
---@block
-CREATE TABLE A(name VARCHAR(16) PRIMARY KEY);
-CREATE TABLE B(name VARCHAR(16) PRIMARY KEY);
-
---@block
-INSERT INTO A
-VALUES ('XYZ'),
-  ('XXX'),
-  ('ZZZ'),
-  ('YYY');
 
 
---@block
-INSERT INTO B
-VALUES('AAA'),
-('BBB'),
-('CCC'),
-('ZZZ');
-
---@block
-SELECT * FROM A UNION SELECT * FROM B;
-
-
---@block
-INSERT INTO A 
-VALUES ('CCC');
-
---@block
-SELECT *
-FROM A INTERSECT 
-SELECT * FROM B;
-
-
---@block
-SELECT phone_id 
-FROM phones_to_orders
-INTERSECT
-SELECT id FROM phones;
-
-
---@block
-SELECT id
-FROM phones
-EXCEPT
-SELECT phone_id 
-FROM phones_to_orders;
-DELETE FROM phones_to_orders WHERE phone_id=10;
