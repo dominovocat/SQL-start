c9ba9c43538f212aa3d2f9a2ed52307a8cd30ec0
danil <minakonn9shkoni@gmail.com>
2022-06-23 16:46:51 +0300

    Add sequelize module

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,5 +1,6 @@
 const { QueryTypes } = require("sequelize");
 const client = require("../models");
+const User = require("../models/user");
 
 module.exports.createUser = async () => {
   
@@ -10,15 +11,24 @@ module.exports.getUserList = async (limit, page) => {
 }
 
 module.exports.getUserById = async (userId) => {
-  const qInt = client.getQueryInterface();
   
-  const foundUsers = await qInt.select(null,'users',{ where:{
-    id:userId,
-  } });
+  // const qInt = client.getQueryInterface();
+
+  // const foundUsers = await qInt.select(null,'users',{ where:{
+  //   id:userId,
+  // } });
 
   // const foundUsers  = await client.query(`
   //   SELECT * FROM "users" WHERE "id" = ${userId}
   // `,{type:QueryTypes.SELECT});
+
+  const foundUsers = await User.findAll({
+    where:{
+      id:userId,
+    }
+  });
+  // const foundUsers = await User.findByPk
+  // const foundUsers = await User.findOne
 
   if (foundUsers.length === 0) {
     return null;
