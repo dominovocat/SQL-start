fca849cc9c4acf3b2b971c29fe82f98cd4fc011a
danil <minakonn9shkoni@gmail.com>
2022-06-23 13:33:46 +0300

    Reconstruct src folder

--- src/migrate.js
+++ src/migrate.js
@@ -1,32 +1,31 @@
-const fs = require("fs/promises");
+const fs = require('fs/promises');
 const path = require('path');
 
 module.exports.migrate = async (client) => {
   await client.query(`
-  CREATE TABLE IF NOT EXISTS migrations
-  (
-    name VARCHAR(128) PRIMARY KEY CHECK(name !='')
-  );
-  `);
-  const migrations = await client.query(`
-    SELECT * FROM migrations;
-
+    CREATE TABLE IF NOT EXISTS migrations
+    (
+      name varchar(128) PRIMARY KEY CHECK(name != '')
+    );
   `);
 
-  const filesNames = await fs.readdir(
-    path.resolve(__dirname,"./migrations/")
-  );
-  for (const fileName of filesNames) {
-    const filepath = path.resolve(__dirname, "./migrations/",fileName);
+  const { rows: migrations } = await client.query(`
+      SELECT * FROM migrations;
+  `);
+
+  const fileNames = await fs.readdir(path.resolve(__dirname, './migrations/'));
+
+  for (const fileName of fileNames) {
+    const filepath = path.resolve(__dirname, './migrations/', fileName);
     const module = require(filepath);
 
     if (!migrations.includes(fileName)) {
       await module.up(client);
       await client.query(`
-    INSERT INTO migrations
-    VALUES
-    ('${fileName}')
-    `);
+        INSERT INTO migrations
+        VALUES
+        ('${fileName}')
+      `);
     }
   }
-};
+}
